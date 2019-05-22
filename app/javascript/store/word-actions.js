0c034a9800d5b3598a46a8c4394e1cc8720be10b
Melanie Matthews <melanie.a.matthews@gmail.com>
2019-05-22 13:41:37 +1000

    Don’t fetch words again.

--- app/javascript/store/word-actions.js
+++ app/javascript/store/word-actions.js
@@ -1,8 +1,11 @@
 import { getAllWords } from '../api/word-api';
 import { startLoading, finishedLoading } from './loading-reducer';
 import { storeWords } from './words-reducer';
+import store from './store';
 
 export const fetchWords = () => dispatch => {
+  if (store.getState().words.length > 0) return;
+
   startLoading('words');
 
   getAllWords().then((words) => {
