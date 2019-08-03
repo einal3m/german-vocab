14b413476899d2b393ac1f8907532d3a6d88595d
Melanie Matthews <melanie.a.matthews@gmail.com>
2019-08-03 14:50:39 +1000

    Alter seed rake task to not create words that are already there.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 require 'csv'
 
 CSV.read("db/words.csv", headers: true, header_converters: :symbol).each do |word|
+  found = Word.find_by(german: word[:german])
+  next if found
+
   Word.create(
     german: word[:german], 
     article: word[:article], 
